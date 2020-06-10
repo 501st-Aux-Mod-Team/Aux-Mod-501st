7db57a9b98e227289342855db9520cdf683fbd4e
Purdy <prdxcm@gmail.com>
2020-06-09 23:46:15 -0500

    update
    added 501st markers

---
+++ addons - Copy/zPurdy'sthings/RD501_Markers/config.cpp
@@ -0,0 +1,322 @@
+class CfgMarkers
+{
+	class 501st_Marker
+	{
+		name = "501st";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\501.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class AvalancheHQ_Marker
+	{
+		name = "Avalanche Company";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\AvalancheHQ.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Avalanche1_Marker
+	{
+		name = "Avalanche 1st Platoon";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Avalancch1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Avalanche2_Marker
+	{
+		name = "Avalanche 2nd Platoon";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Avalanche2.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Kryat_Marker
+	{
+		name = "Kryat";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Kryat.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Cyclone_Marker
+	{
+		name = "Cyclone Company";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Cyclone.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Cyclone1_Marker
+	{
+		name = "Cyclone 1st Platoon";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Cyclone1.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Cyclone3_Marker
+	{
+		name = "Cyclone 3rd Platoon";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Cyclone3.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Acklay_Marker
+	{
+		name = "Acklay";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Acklay.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Mynock_Marker
+	{
+		name = "Mynock";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Mynock.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Razor_Marker
+	{
+		name = "Razor";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Razor.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Zeta_Marker
+	{
+		name = "Zeta";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Zeta.paa";
+		color[] = {1,1,1,1};
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class RTO_Marker
+	{
+		name = "RTO";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\RTO.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Medic_Marker
+	{
+		name = "Medic";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Medic.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class LAAT_Marker
+	{
+		name = "LAAT";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\LAAT.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class ARC170_Marker
+	{
+		name = "Arc-170";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Arc170.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Saber_Marker
+	{
+		name = "Saber";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Saber.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class CIS_Marker
+	{
+		name = "CIS";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\CIS.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class B1_Marker
+	{
+		name = "B1";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\B1.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class B1Heavy_Marker
+	{
+		name = "B1 Heavy";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\B1Heavy.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Indepent_Marker
+	{
+		name = "B1 Independent";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\IndepentB1.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class B2_Marker
+	{
+		name = "B2";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\B2.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class BX_Marker
+	{
+		name = "BX";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\BX.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Magna_Marker
+	{
+		name = "Magna";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Magna.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class DSD_Marker
+	{
+		name = "DSD";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\DSD.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Deka_Marker
+	{
+		name = "Deka";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Deka.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class HMP_Marker
+	{
+		name = "HMP";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\HMP.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class Vulture_Marker
+	{
+		name = "Vulture";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\Vulture.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class AAT_Marker
+	{
+		name = "AAT";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\AAT.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class KingAAT_Marker
+	{
+		name = "King AAT";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\KingAAT.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class MTT_Marker
+	{
+		name = "MTT";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\MTT.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
+	class TomasTheMTT_Marker
+	{
+		name = "Tomas The MTT";
+		icon = "\Aux-Mod-501st\Addons\RD501_Markers\markers\TomasTheMTT.paa";
+		color[] = { 1,1,1,1 };
+		size = 32;
+		shadow = 0;
+		scope = 2;
+		markerClass = "501st Markers";
+	};
