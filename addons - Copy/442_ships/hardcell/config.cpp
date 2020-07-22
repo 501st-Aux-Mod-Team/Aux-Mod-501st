bd358c2fdd4a4e2528ed735975957d0f59c270d2
Erliens <erlien.unitrax@gmail.com>
2020-07-22 20:32:20 +0100

    Working 442 ships added with categories.

--- addons - Copy/442_ships/hardcell/config.cpp
+++ addons - Copy/442_ships/hardcell/config.cpp
@@ -1,19 +1,20 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: 442_ships\hardcell\config.bin
-//Produced from mikero's Dos Tools Dll version 7.46
-//https://bytex.market/products/item/weodpphdknnzm70o0h8q/Mikero%27s%20Dos%20Tools
-//'now' is Thu Jun 25 16:36:15 2020 : 'file' last modified on Sun Jun 07 11:07:52 2020
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class cfgpatches
 {
 	class 442_hardcell
 	{
-		requiredAddons[] = {};
-		weapons[] = {};
-		units[] = {"hardcell_base","hardcell_body","hardcell_engine1","hardcell_engine2","hardcell_engine3","hardcell_leg1","hardcell_leg2","hardcell_leg3"};
+		requiredAddons[]={};
+		weapons[]={};
+		units[]=
+		{
+			"hardcell_base",
+			"hardcell_body",
+			"hardcell_engine1",
+			"hardcell_engine2",
+			"hardcell_engine3",
+			"hardcell_leg1",
+			"hardcell_leg2",
+			"hardcell_leg3"
+		};
 	};
 };
 class Eventhandlers;
@@ -32,131 +33,213 @@ class CfgVehicles
 	class Items_base_F;
 	class hardcell_base: StaticShip
 	{
-		author = "kripto202";
-		model = "\442_ships\hardcell\base.p3d";
-		reversed = 0;
-		scope = 2;
-		scopecurator = 1;
-		editorpreview = "";
-		editorcategory = "kobra";
-		editorsubcategory = "442_ships";
-		vehicleClass = "Structures_Military";
-		icon = "iconObject_1x1";
-		displayname = "Hardcell Transport";
-		mapsize = 3000;
-		destrType = 0;
-		featureType = 2;
-		multistructureparts[] = {{"hardcell_body","body"},{"hardcell_engine1","engine1"},{"hardcell_engine2","engine2"},{"hardcell_engine3","engine3"},{"hardcell_leg1","leg1"},{"hardcell_leg2","leg2"},{"hardcell_leg3","leg3"}};
+		author="kripto202";
+		model="\442_ships\hardcell\base.p3d";
+		reversed=0;
+		scope=2;
+		scopecurator=1;
+		editorpreview="";
+		editorcategory="kobra";
+		editorsubcategory="442_ships";
+		vehicleClass="Structures_Military";
+		icon="iconObject_1x1";
+		displayname="Hardcell Transport";
+		mapsize=3000;
+		destrType=0;
+		featureType=2;
+		multistructureparts[]=
+		{
+			
+			{
+				"hardcell_body",
+				"body"
+			},
+			
+			{
+				"hardcell_engine1",
+				"engine1"
+			},
+			
+			{
+				"hardcell_engine2",
+				"engine2"
+			},
+			
+			{
+				"hardcell_engine3",
+				"engine3"
+			},
+			
+			{
+				"hardcell_leg1",
+				"leg1"
+			},
+			
+			{
+				"hardcell_leg2",
+				"leg2"
+			},
+			
+			{
+				"hardcell_leg3",
+				"leg3"
+			}
+		};
 		class Eventhandlers
 		{
-			init = "_this call BIS_fnc_Destroyer01Init;";
-			attributesChanged3DEN = "_this call BIS_fnc_Destroyer01PosUpdate;";
-			dragged3DEN = "_this call BIS_fnc_Destroyer01PosUpdate;";
-			registeredToWorld3DEN = "_this call BIS_fnc_Destroyer01EdenInit;";
-			unregisteredFromWorld3DEN = "_this call BIS_fnc_Destroyer01EdenDelete;";
+			init="_this call BIS_fnc_Destroyer01Init;";
+			attributesChanged3DEN="_this call BIS_fnc_Destroyer01PosUpdate;";
+			dragged3DEN="_this call BIS_fnc_Destroyer01PosUpdate;";
+			registeredToWorld3DEN="_this call BIS_fnc_Destroyer01EdenInit;";
+			unregisteredFromWorld3DEN="_this call BIS_fnc_Destroyer01EdenDelete;";
 		};
 	};
 	class hardcell_hull_base: House_F
 	{
-		reversed = 0;
-		scope = 1;
-		scopeCurator = 0;
-		author = "kripto202";
-		displayname = "hardcell_hull_base";
-		editorpreview = "";
-		editorcategory = "kobra";
-		editorsubcategory = "442_ships_parts";
-		vehicleClass = "Structures_Military";
-		icon = "iconObject_1x1";
-		mapsize = 70;
-		model = "\442_ships\hardcell\1.p3d";
-		armor = 1000000000000000000;
-		hideUnitInfo = 1;
-		nameSound = "ship";
-		featureType = 2;
-		class Hitpoints{};
-		class DestructionEffects{};
+		reversed=0;
+		scope=1;
+		scopeCurator=0;
+		author="kripto202";
+		displayname="hardcell_hull_base";
+		editorpreview="";
+		editorcategory="kobra";
+		editorsubcategory="442_ships_parts";
+		vehicleClass="Structures_Military";
+		icon="iconObject_1x1";
+		mapsize=70;
+		model="\442_ships\hardcell\1.p3d";
+		armor=1000000000000000000;
+		hideUnitInfo=1;
+		nameSound="ship";
+		featureType=2;
+		class Hitpoints
+		{
+		};
+		class DestructionEffects
+		{
+		};
 		class Damage
 		{
-			tex[] = {};
-			mat[] = {};
+			tex[]={};
+			mat[]={};
 		};
-		class AnimationSources{};
-		class UserActions{};
+		class AnimationSources
+		{
+		};
+		class UserActions
+		{
+		};
 	};
 	class hardcell_body: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell body";
-		editorpreview = "";
-		model = "\442_ships\hardcell\body.p3d";
-		hiddenselections[] = {"upper"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\upper_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell body";
+		editorpreview="";
+		model="\442_ships\hardcell\body.p3d";
+		hiddenselections[]=
+		{
+			"upper"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\upper_co.paa"
+		};
 	};
 	class hardcell_engine1: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell engine1";
-		editorpreview = "";
-		model = "\442_ships\hardcell\engine1.p3d";
-		hiddenselections[] = {"lower"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\lower_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell engine1";
+		editorpreview="";
+		model="\442_ships\hardcell\engine1.p3d";
+		hiddenselections[]=
+		{
+			"lower"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\lower_co.paa"
+		};
 	};
 	class hardcell_engine2: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell engine2";
-		editorpreview = "";
-		model = "\442_ships\hardcell\engine2.p3d";
-		hiddenselections[] = {"lower"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\lower_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell engine2";
+		editorpreview="";
+		model="\442_ships\hardcell\engine2.p3d";
+		hiddenselections[]=
+		{
+			"lower"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\lower_co.paa"
+		};
 	};
 	class hardcell_engine3: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell engine3";
-		editorpreview = "";
-		model = "\442_ships\hardcell\engine3.p3d";
-		hiddenselections[] = {"lower"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\lower_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell engine3";
+		editorpreview="";
+		model="\442_ships\hardcell\engine3.p3d";
+		hiddenselections[]=
+		{
+			"lower"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\lower_co.paa"
+		};
 	};
 	class hardcell_leg1: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell leg1";
-		editorpreview = "";
-		model = "\442_ships\hardcell\leg1.p3d";
-		hiddenselections[] = {"lower"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\lower_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell leg1";
+		editorpreview="";
+		model="\442_ships\hardcell\leg1.p3d";
+		hiddenselections[]=
+		{
+			"lower"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\lower_co.paa"
+		};
 	};
 	class hardcell_leg2: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell leg2";
-		editorpreview = "";
-		model = "\442_ships\hardcell\leg2.p3d";
-		hiddenselections[] = {"lower"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\lower_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell leg2";
+		editorpreview="";
+		model="\442_ships\hardcell\leg2.p3d";
+		hiddenselections[]=
+		{
+			"lower"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\lower_co.paa"
+		};
 	};
 	class hardcell_leg3: hardcell_hull_base
 	{
-		scope = 2;
-		scopeCurator = 2;
-		displayname = "hardcell leg3";
-		editorpreview = "";
-		model = "\442_ships\hardcell\leg3.p3d";
-		hiddenselections[] = {"lower"};
-		hiddenselectionstextures[] = {"442_ships\hardcell\data\lower_co.paa"};
+		scope=2;
+		scopeCurator=2;
+		displayname="hardcell leg3";
+		editorpreview="";
+		model="\442_ships\hardcell\leg3.p3d";
+		hiddenselections[]=
+		{
+			"lower"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\hardcell\data\lower_co.paa"
+		};
 	};
-};
-class cfgMods
-{
-	author = "kripto202";
-	timepacked = "1591528071";
 };
--- addons - Copy/442_ships/quasar/config.cpp
+++ addons - Copy/442_ships/quasar/config.cpp
@@ -1,20 +1,14 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: 442_ships\quasar\config.bin
-//Produced from mikero's Dos Tools Dll version 7.46
-//https://bytex.market/products/item/weodpphdknnzm70o0h8q/Mikero%27s%20Dos%20Tools
-//'now' is Thu Jun 25 16:36:17 2020 : 'file' last modified on Sun Jun 07 11:07:52 2020
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class CfgPatches
 {
 	class 442_quasar
 	{
-		requiredAddons[] = {};
-		weapons[] = {};
-		units[] = {"442_quasar"};
-		author = "kripto202";
+		requiredAddons[]={};
+		weapons[]={};
+		units[]=
+		{
+			"442_quasar",
+		};
+		author="kripto202";
 	};
 };
 class Eventhandlers;
@@ -33,20 +27,41 @@ class CfgVehicles
 	class Items_base_F;
 	class 442_quasar: House_F
 	{
-		scope = 2;
-		scopecurator = 2;
-		author = "kripto202";
-		displayName = "Quasar Fire Class Cruiser/Carrier";
-		model = "\442_ships\quasar\quasar.p3d";
-		editorcategory = "kobra";
-		editorsubcategory = "442_ships";
-		vehicleClass = "Structures";
-		hiddenselections[] = {"body1","body2","engine","guns","interior1","interior2","interior3","interior4","door1","door2","frame"};
-		hiddenselectionstextures[] = {"442_ships\quasar\data\body1_co.paa","442_ships\quasar\data\body2_co.paa","442_ships\quasar\data\engine_co.paa","442_ships\quasar\data\guns_co.paa","442_ships\quasar\data\interior1_co.paa","442_ships\quasar\data\interior2_co.paa","442_ships\quasar\data\interior3_co.paa","442_ships\quasar\data\interior4_co.paa","442_structures\hallway\data\doors_co.paa","442_structures\hallway\data\doors_co.paa","442_structures\hallway\data\frame_co.paa"};
+		scope=2;
+		scopecurator=2;
+		author="kripto202";
+		displayName="Quasar Fire Class Cruiser/Carrier";
+		model="\442_ships\quasar\quasar.p3d";
+		editorcategory="kobra";
+		editorsubcategory="442_ships";
+		vehicleClass="Structures";
+		hiddenselections[]=
+		{
+			"body1",
+			"body2",
+			"engine",
+			"guns",
+			"interior1",
+			"interior2",
+			"interior3",
+			"interior4",
+			"door1",
+			"door2",
+			"frame"
+		};
+		hiddenselectionstextures[]=
+		{
+			"442_ships\quasar\data\body1_co.paa",
+			"442_ships\quasar\data\body2_co.paa",
+			"442_ships\quasar\data\engine_co.paa",
+			"442_ships\quasar\data\guns_co.paa",
+			"442_ships\quasar\data\interior1_co.paa",
+			"442_ships\quasar\data\interior2_co.paa",
+			"442_ships\quasar\data\interior3_co.paa",
+			"442_ships\quasar\data\interior4_co.paa",
+			"442_structures\hallway\data\doors_co.paa",
+			"442_structures\hallway\data\doors_co.paa",
+			"442_structures\hallway\data\frame_co.paa",
+		};
 	};
-};
-class cfgMods
-{
-	author = "kripto202";
-	timepacked = "1591528071";
 };
