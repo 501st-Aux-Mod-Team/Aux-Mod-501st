4a960c9a6b9bebec740bfbb33c58735377441167
Erliens <erlien.unitrax@gmail.com>
2020-07-28 21:38:22 +0100

    Vehicles in Zeus and Glaive wheel damage script

--- addons - Copy/RD501_Vehicles/land/Mynock/common_stuff_mynock.hpp
+++ addons - Copy/RD501_Vehicles/land/Mynock/common_stuff_mynock.hpp
@@ -2,6 +2,7 @@ class EventHandlers;
 class EventHandlers: DefaultEventhandlers
 {
 	fired = "_this call (uinamespace getviariable 'BIS_fnc_effectFired');";
+	init="[_this select 0] execVM 'RD501_Vehicles\_init_functions\wheel_dmg.sqf';";
 };
 smokeLauncherGrenadeCount=8;
 smokeLauncherVelocity=14;
