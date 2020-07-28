77a8e9a4fc10d961ab42d5bc5b94bbaa6e0f8875
Florian Steinschorn <e1227109@student.tuwien.ac.at>
2020-07-28 09:49:37 +0200

    arc170x_restructuring

---
+++ addons - Copy/RD501_Vehicles/air/ARC_170X/arc170_inheritance.hpp
@@ -0,0 +1,25 @@
+class 3as_arc_170_base;
+class 3as_arc_170_razor: 3as_arc_170_base
+{
+	class Components;
+};
+class 3as_arc_170_blue: 3as_arc_170_base
+{
+	class Components;
+};
+class 3as_arc_170_green: 3as_arc_170_base
+{
+	class Components;
+};
+class 3as_arc_170_Orange: 3as_arc_170_base
+{
+	class Components;
+};
+class 3as_arc_170_yellow: 3as_arc_170_base
+{
+	class Components;
+};
+class 3as_arc_170_red: 3as_arc_170_base
+{
+	class Components;
+};
