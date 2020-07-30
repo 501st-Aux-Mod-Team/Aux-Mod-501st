162ac1980293730e94f364bf57b3f203f4c518d5
Erliens <erlien.unitrax@gmail.com>
2020-07-30 19:23:35 +0100

    501st HUD added

--- addons - Copy/RD501_Vehicles/air/ARC_170X/config.cpp
+++ addons - Copy/RD501_Vehicles/air/ARC_170X/config.cpp
@@ -29,31 +29,37 @@ class CfgVehicles
 	class macro_new_vehicle(arc_170,razor): 3as_arc_170_razor
 	{
 		displayName="ARC-170 (Razor)";
+		#include "../../common/universal_mfd.hpp"
 		#include "arc170_common.hpp"
 	};
 	class macro_new_vehicle(arc_170,blue): 3as_arc_170_blue
 	{
 		displayName="ARC-170 (Blue)";
+		#include "../../common/universal_mfd.hpp"
 		#include "arc170_common.hpp"
 	};
 	class macro_new_vehicle(arc_170,green): 3as_arc_170_green
 	{
 		displayName="ARC-170 (Green)";
+		#include "../../common/universal_mfd.hpp"
 		#include "arc170_common.hpp"
 	};
 	class macro_new_vehicle(arc_170,orange): 3as_arc_170_Orange
 	{
 		displayName="ARC-170 (Orange)";
+		#include "../../common/universal_mfd.hpp"
 		#include "arc170_common.hpp"
 	};
 	class macro_new_vehicle(arc_170,yellow): 3as_arc_170_yellow
 	{
 		displayName="ARC-170 (Yellow)";
+		#include "../../common/universal_mfd.hpp"
 		#include "arc170_common.hpp"
 	};
 	class macro_new_vehicle(arc_170,red): 3as_arc_170_red
 	{
 		displayName="ARC-170 (Red)";
+		#include "../../common/universal_mfd.hpp"
 		#include "arc170_common.hpp"
 	};
 };
