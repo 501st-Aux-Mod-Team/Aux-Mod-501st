95f491121a8abb4a01b71706b9de21f76ac84937
m3ales <mattfdavies12@gmail.com>
2020-10-23 00:11:52 +0200

    Add compositions (Groups > Neutral in Zeus)

---
+++ addons - Copy/RD501_Compositions/data/cis/ships/example_ship.hpp
@@ -0,0 +1,4 @@
+class Object0	{side = 8; vehicle = "RD501_resuppy_box_platoon_medical"; rank = ""; position[] = {0,0,4.24385e-005}; dir = -349.901;};
+class Object1	{side = 8; vehicle = "RD501_resuppy_box_small_medical"; rank = ""; position[] = {2.96729,1.5437,-9.53674e-007}; dir = -349.901;};
+class Object2	{side = 8; vehicle = "RD501_resuppy_box_small_ammo"; rank = ""; position[] = {2.79004,-0.179443,0}; dir = 10.0985;};
+class Object3	{side = 8; vehicle = "RD501_resuppy_box_platoon_ammo"; rank = ""; position[] = {5.65576,-1.16553,4.29153e-005}; dir = -349.901;};
