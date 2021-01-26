b068ab2c92f72a5385d0b7fe7e0e374f5061805a
Erliens <erlien.unitrax@gmail.com>
2021-01-26 10:07:37 +0000

    Jumppack LR fix + Droppack

--- addons - Copy/RD501_Jumppack/config.cpp
+++ addons - Copy/RD501_Jumppack/config.cpp
@@ -39,6 +39,8 @@ class CfgVehicles
 	{
 		scope=2;
 		displayname = "[501st] AB Jumppack 01";
+		tf_dialog="JLTS_clone_lr_programmer_radio_dialog";
+		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
 		tf_encryptionCode="tf_west_radio_code";
 		tf_subtype="digital_lr";
 		tf_range=35000;
@@ -85,55 +87,6 @@ class CfgVehicles
 			{
 				"Short Jump",
 				{12,5,30,0,1,0}
-			}
-		};
-	};
-		class RD501_JLTS_Clone_jumppack_mc_noLR : JLTS_Clone_jumppack_JT12
-		{
-		scope = 2;
-		displayname = "[501st] AB Jumppack 01 no LR";
-		maximumLoad = 700;
-		JLTS_isJumppack = 0;
-		RD501_jumppack_is_jumppack = 1;
-		RD501_jumppack_spam_delay = 1;
-		RD501_jumppack_energy_capacity = 100;
-		RD501_jumppack_recharge = 4;
-		RD501_jumppack_jump_effect_script = "RD501_jumppack_fnc_effect_jt_21";
-		RD501_jumppack_effect_points[] =
-		{
-
-			{
-				"spine3",
-					{0, -0.30000001, -0.1}
-			}
-		};
-		RD501_jumppack_sound_ignite[] =
-		{
-			"RD501_Jumppack\sounds\cdv21Start.ogg"
-		};
-		RD501_jumppack_sound_land[] =
-		{
-			"RD501_Jumppack\sounds\cdv21End.ogg"
-		};
-		RD501_jumppack_sound_idle[] =
-		{
-			"RD501_Jumppack\sounds\cdv21Idle.ogg"
-		};
-		RD501_jumppack_skin_group[] =
-		{
-			"default_texture_group"
-		};
-		RD501_jumppack_jump_types[] =
-		{
-
-			{
-				"Forward Jump",
-					{12, 20, 50, 0, 0, 0}
-			},
-
-			{
-				"Short Jump",
-					{12, 5, 30, 0, 1, 0}
 			}
 		};
 	};
@@ -190,6 +143,29 @@ class CfgVehicles
 			}
 		};
 	};
+class macro_jumppackClass(neutral,jt21_LTU):RD501_JLTS_Clone_jumppack_mc
+	{
+		scope=2;
+		displayname= "[501st] JT21 Droppack";
+		maximumload = 0;
+		macro_energyCap=4;
+		macro_rechargeRate=0;
+		maximumload = 50;
+		macro_types_of_jumps = 
+		{
+			{"Short Jump",{12,5,1,0,1,0}}
+		};
+
+		model="\MRC\JLTS\characters\CloneArmor\CloneJumppack.p3d";
+		hiddenSelections[]=
+		{
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\MRC\JLTS\characters\CloneArmor\data\Clone_jumppack_co.paa"
+		};
+	};
 	class macro_jumppackClass(neutral,titan_mkII):RD501_JLTS_Clone_jumppack_mc2
 	{
 		scope=2;
@@ -221,51 +197,6 @@ class CfgVehicles
 			{"LEET Jump",{1337,7,1,0,1,1}}
 		};
 	};	
-	class RD501_JLTS_Clone_jumppack_hq_1_noLR: RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (HQ 1) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_1HQ.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_hq_2_noLR: RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (HQ 2) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_2HQ.paa.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_6_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (HQ 6) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_6HQ.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_1_1_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (1-1) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_1-1.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_1_2_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (1-2) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_1-2.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_1_3_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (1-3) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_1-3.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_2_1_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (2-1) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_2-1.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_2_2_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (2-2) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_2-2.paa"};
-	};
-	class RD501_JLTS_Clone_jumppack_mc_2_3_noLR : RD501_JLTS_Clone_jumppack_mc_noLR
-	{
-		displayname = "[501st] AB Jumppack (2-3) no LR";
-		hiddenSelectionsTextures[] = {"RD501_Jumppack\textures\acklay_skin_set\JT-12_2-3.paa"};
-	};		
 	class RD501_JLTS_Clone_jumppack_mc_2 : RD501_JLTS_Clone_jumppack_mc {
 		displayname = "[501st] AB Jumppack 02";
 		hiddenSelectionsTextures[] = { "\MRC\JLTS\characters\CloneLegions\data\Clone_501stTrooper_jumppack_JT12_co.paa" };
